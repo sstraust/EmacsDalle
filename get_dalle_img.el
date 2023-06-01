724d0e6280909b8f30292897874a7a053b162759
sam <sam@sams-mbp.myfiosgateway.com>
2023-06-01 14:26:23 -0400

    add namespace prefixes to internal functions

--- get_dalle_img.el
+++ get_dalle_img.el
@@ -7,20 +7,20 @@
 (defconst PEXELS-TOKEN "")
 (defconst IMAGE-COUNT 5)
 
-(defun get-request-args-list (params)
+(defun get-dalle-img-get-request-args-list (params)
   (concat "?"
 	  (combine-and-quote-strings
 	   (mapcar (lambda(x) (concat (substring (symbol-name (car x)) 1) "=" (cadr x))) (seq-partition params 2))
 	   "&")))
 
-(defun send-json-request (url method params token)
+(defun get-dalle-img-send-json-request (url method params token)
   "Send a JSON request to the specified URL with bearer token."
   (let* ((url-request-method method)
          (url-request-extra-headers `(("Authorization" . ,token)
                                       ("Content-Type" . "application/json")))
          (url-request-data (json-encode params))
 	 (url (if (equal method "GET")
-		  (concat url "/" (get-request-args-list params))
+		  (concat url "/" (get-dalle-img-get-request-args-list params))
 		url))
          (buffer (url-retrieve-synchronously url)))
     (with-current-buffer buffer
@@ -28,27 +28,27 @@
       (re-search-forward "^$")
       (json-read))))
 
-(defun request-dalle-images (prompt n)
+(defun get-dalle-img-request-dalle-images (prompt n)
   (let* ((params (list
 		  :prompt prompt
 		  :n n))
 	 
-	 (json-response (send-json-request OPENAI-DALLE-API-URL "POST" params (concat "Bearer " OPENAI-DALLE-TOKEN)))
+	 (json-response (get-dalle-img-send-json-request OPENAI-DALLE-API-URL "POST" params (concat "Bearer " OPENAI-DALLE-TOKEN)))
 	 (url-list (cdr (assoc 'data json-response))))
     url-list))
 
-(defun request-pexels-images (prompt n)
+(defun get-dalle-img-request-pexels-images (prompt n)
   (let* ((params (list
 		   :query prompt
 		   :per_page (number-to-string n)
 		   :page "1"))
-	 (json-response (send-json-request PEXELS-URL "GET" params PEXELS-TOKEN))
+	 (json-response (get-dalle-img-send-json-request PEXELS-URL "GET" params PEXELS-TOKEN))
 	 (url-list (cdr (assoc 'photos  json-response))))
     (vconcat (mapcar (lambda (photo)
 	  (list (cons 'url (cdr (assoc 'large (cdr (assoc 'src photo)))))))
 	  url-list))))
 
-(defun insert-image-to-choose-image-menu (&optional url i)
+(defun get-dalle-img-insert-image-to-choose-image-menu (&optional url i)
   (unless url (setq url (url-get-url-at-point)))
   (unless url
     (error "Couldn't find URL."))
@@ -65,13 +65,13 @@
 	   (insert "\n"))
       (kill-buffer buffer))))
 
-(defun choose-image-from-menu (image-url-list)
+(defun get-dalle-img-choose-image-from-menu (image-url-list)
   (let* ((index 1))
   (switch-to-buffer "*dalle_output_buffer*")
   (erase-buffer)
   (mapcar (lambda (image-url)
 	    (progn
-	      (insert-image-to-choose-image-menu (cdr (assoc 'url image-url)) index)
+	      (get-dalle-img-insert-image-to-choose-image-menu (cdr (assoc 'url image-url)) index)
 	      (setq index (1+ index))))
 	  image-url-list)
   (setq chosen-number (read-number "Choose which image to use (enter a number): "))
@@ -80,8 +80,8 @@
 
 
 (defun generate-featured-blog-image-dalle-helper (prompt n)
-  (let ((images (request-dalle-images prompt n)))
-    (choose-image-from-menu images)))
+  (let ((images (get-dalle-img-request-dalle-imagesxs prompt n)))
+    (get-dalle-img-choose-image-from-menu images)))
 
 (defun generate-featured-blog-image-dalle ()
   (interactive)
@@ -89,9 +89,8 @@
     (generate-featured-blog-image-dalle-helper prompt IMAGE-COUNT)))
 
 (defun generate-featured-blog-image-pexels-helper (prompt n)
-  (interactive)
-  (let ((images (request-pexels-images prompt n)))
-    (choose-image-from-menu images)))
+  (let ((images (get-dalle-img-request-pexels-images prompt n)))
+    (get-dalle-img-choose-image-from-menu images)))
 
 (defun generate-featured-blog-image-pexels ()
   (interactive)
